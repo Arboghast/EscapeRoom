5a206d1410f3ad3fc961421aa286ca9152053726
Sadman Hossain <ahossain5363@bths.edu>
2018-01-28 14:30:42 -0500

    yes

--- 3dexperiments/SadmansPackage/S/example.java
+++ 3dexperiments/SadmansPackage/S/example.java
@@ -5,6 +5,8 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.Sphere;
 import javafx.stage.Stage;
 
@@ -12,13 +14,28 @@ public class example extends Application{
 	 @Override
 	   public void start(Stage primaryStage) {
 	      
-	      Sphere sphere = new Sphere(200);
-	      PhongMaterial material1 = new PhongMaterial();
-	      material1.setDiffuseColor(Color.BLUE);
-	      material1.setSpecularColor(Color.LIGHTBLUE);
-	      material1.setSpecularPower(10.0);
-	      sphere.setMaterial(material1);
-	      Group root=new Group(sphere);
+		 final PhongMaterial redMaterial = new PhongMaterial();
+	       redMaterial.setSpecularColor(Color.ORANGE);
+	       redMaterial.setDiffuseColor(Color.RED);
+	 
+	       final PhongMaterial blueMaterial = new PhongMaterial();
+	       blueMaterial.setDiffuseColor(Color.BLUE);
+	       blueMaterial.setSpecularColor(Color.LIGHTBLUE);
+
+	       final PhongMaterial greyMaterial = new PhongMaterial();
+	       greyMaterial.setDiffuseColor(Color.DARKGREY);
+	       greyMaterial.setSpecularColor(Color.GREY);
+
+	       final Box red = new Box(400, 400, 400);
+	       red.setMaterial(redMaterial);
+	 
+	       final Sphere blue = new Sphere(200);
+	       blue.setMaterial(blueMaterial);
+	 
+	       final Cylinder grey = new Cylinder(5, 100);
+	       grey.setMaterial(greyMaterial);
+	       
+	      Group root=new Group(red,blue,grey);
 
 	      Scene scene = new Scene(root, 700, 700);
 
