c2ae84f91027d3194a0e057ecd0e1fe321264c4a
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2018-02-08 12:59:28 -0500

    pushy

---
+++ help/src/hello/buttonPuzzles.java
@@ -0,0 +1,84 @@
+package hello;
+import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.input.controls.Trigger;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+
+public class buttonPuzzles extends SimpleApplication {
+	
+	private Geometry geom;
+	private final static Trigger mouseClick = new MouseButtonTrigger(MouseInput.BUTTON_LEFT);
+	private final static String  MAPPING_COLOR = "Toggle color";
+	private boolean toggled =  false;
+	private boolean isCreated = false;
+	int trigger = 0;
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+		inputManager.addMapping(MAPPING_COLOR, mouseClick);
+		inputManager.addListener(actionsListener, new String[]{MAPPING_COLOR});
+		for(int i = 0; i <= 7; i++)
+		{	
+			Box mesh = new Box(3, 3, 3);
+	        geom = new Geometry("Box", mesh);
+	        Material mat = new Material(assetManager,
+	                "Common/MatDefs/Misc/Unshaded.j3md");
+	        mat.setColor("Color", ColorRGBA.Blue);
+	        geom.setMaterial(mat);
+	        rootNode.attachChild(geom);
+	        int x = 15;
+	        geom.setLocalTranslation(new Vector3f(x * i,0,0));
+		}
+		
+	}
+	private ActionListener actionsListener = new ActionListener() {
+   	 public void onAction(String name, boolean isPressed, float tpf) {
+        	System.out.println("Mapping detected (discrete): "+name );
+        	if(!isPressed)
+        	{
+        		if (!isCreated) {
+					Sphere sphereMesh = new Sphere(32, 32, 2f);
+					geom = new Geometry("Sphere", sphereMesh);
+					Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+					mat.setColor("Color", ColorRGBA.Red);
+					geom.setMaterial(mat);
+					rootNode.attachChild(geom);
+					isCreated = true;
+					trigger++;
+				}
+				if (!toggled) {
+					//geom.getMaterial().setColor("Color", ColorRGBA.Red);
+					toggled = true;
+					geom.getMaterial().setColor("Color", ColorRGBA.Red);
+					long i = System.nanoTime();
+					System.out.println(i);
+					trigger++;
+
+				} else {
+					toggled = false;
+					geom.getMaterial().setColor("Color", ColorRGBA.Blue);
+					long i = System.nanoTime();
+					System.out.println(i);
+					trigger++;
+
+				} 
+        	}
+   	 }
+   };
+	public static void main(String[] args){
+    	buttonPuzzles app = new buttonPuzzles();
+        app.start(); // start the game
+
+    }
+
+}
