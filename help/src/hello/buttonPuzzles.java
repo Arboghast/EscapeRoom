3ae75ed2c6a79e0b93f61e67f806124a805adef7
zashim <zashim@192.168.2.6>
2018-02-08 21:28:14 -0500

    pushy

--- help/src/hello/buttonPuzzles.java
+++ help/src/hello/buttonPuzzles.java
@@ -12,6 +12,7 @@ import com.jme3.input.controls.Trigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Sphere;
@@ -30,8 +31,6 @@ public class buttonPuzzles extends SimpleApplication {
 		// TODO Auto-generated method stub
 		inputManager.addMapping(MAPPING_COLOR, mouseClick);
 		inputManager.addListener(actionsListener, new String[]{MAPPING_COLOR});
-		for(int i = 0; i <= 7; i++)
-		{	
 			Box mesh = new Box(3, 3, 3);
 	        geom = new Geometry("Box", mesh);
 	        Material mat = new Material(assetManager,
@@ -40,9 +39,8 @@ public class buttonPuzzles extends SimpleApplication {
 	        geom.setMaterial(mat);
 	        rootNode.attachChild(geom);
 	        int x = 15;
-	        geom.setLocalTranslation(new Vector3f(x * i,0,0));
-	        boxes.add(geom);
-		}
+	        //geom.setLocalTranslation(new Vector3f(0,0,15));
+	        //boxes.add(geom);
 		
 	}
 	private ActionListener actionsListener = new ActionListener() {
@@ -50,14 +48,7 @@ public class buttonPuzzles extends SimpleApplication {
         	System.out.println("Mapping detected (discrete): "+name );
         	if(!isPressed)
         	{
-        		for(int i = 0; i <= boxes.size(); i++)
-        		{
-        			Geometry abox = boxes.get(i);
-        			Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-					mat.setColor("Color", ColorRGBA.Red);
-					abox.setMaterial(mat);
-					rootNode.attachChild(abox);
-        		}
+        		rootNode.detachChild(geom);
         	}
         		
    	 }
