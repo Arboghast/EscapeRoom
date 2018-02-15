daf260968eb8013db4ba5618f5aca40bac767d39
BT_1N3_10 <BT_1N3_10@BT_1N3_10.bktech.local>
2018-02-15 12:56:26 -0500

    dddd

--- GeneralProject/src/mygame/ScreenManager.java
+++ GeneralProject/src/mygame/ScreenManager.java
@@ -54,7 +54,7 @@ public class ScreenManager extends AbstractAppState implements ScreenController 
 
 		File CSV = new File("EscapeRoomScoreSheet.csv");     //Receives User input and adds it along with minutes and seconds to the CSV file
 		CSVReader tracker = new CSVReader(CSV);
-		tracker.writeToCSV(input, this.seconds, this.minutes);
+		tracker.writeToCSV(input, this.minutes, this.seconds);
 
 		app.scoreboard = true;
 		nifty.gotoScreen("scoreboard");
